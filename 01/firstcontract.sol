fcb70171e68818e2f5d2d92af2eed6e85d838071
jiangyuanyuan <jiangyuanyuan@gmail.com>
2018-12-10 11:04:47 +0800

    null

---
+++ 01/firstcontract.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.4;
+
+contract firstcontract{
+	string private name;
+	uint private age;
+
+	function setName(string newName) public {
+		name = newName;
+	}
+
+	function getName() public view returns (string){
+		return name;
+	}
+
+	function setAge (uint newAge)public{
+		age = newAge;
+	}
+
+	function getAge() public view returns (uint){
+		return age;
+	}
+}
